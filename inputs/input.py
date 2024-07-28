e5556b98d1713a74e10421e2fa66cf7d8fe15da2
Enzo <enzo192@gmail.com>
2024-07-28 08:41:57 -0500

    resolviendo conflicto

--- inputs/input.py
+++ inputs/input.py
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-#pedirle un dato al usuario
-nombre = input("dime tu nombre: ")
-
-#mostrar dato
-=======
 
 #pedirle un dato al ususario, siempre es un string
 nombre = input("dime tu nombre: ")
 
 #mostrando el dato
->>>>>>> 34814eded45900efdac7dad6e15c99681d9201fe
 print(f"el nombre es: {nombre}")
